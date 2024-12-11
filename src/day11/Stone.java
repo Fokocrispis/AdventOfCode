cc6499e554fbd5d5044811d7e43c614275fd9677
Fokocrispis <alec.klamroth@gmail.com>
2024-12-11 20:24:26 +0100

    Add Day 11 part 2

--- src/day11/Stone.java
+++ src/day11/Stone.java
@@ -1,53 +1,66 @@
 package day11;
 
-public class Stone {
-	public static final String EVEN = "Even";
-	public static final String ODD = "Odd";
-	public static final String ZERO = "Zero";
-	
-	private long value;
-	private String type;
+import java.util.HashMap;
+import java.util.Map;
 
-	public Stone(long value) {
-		this.value = value;
-		
-		if(value==0) {
-			type = ZERO;
-		}
-		else if(String.valueOf(value).length()%2==0) {
-			type = EVEN;
-		}
-		else {
-			type = ODD;
-		}
-	}
-	
-	public Tuple split() {
-		String temp = Long.toString(value);
-		String stone1 = temp.substring(0, temp.length()/2);
-		String stone2 = temp.substring(temp.length()/2);
-		
-		return new Tuple<Long, Long>(Long.valueOf(stone1), Long.valueOf(stone2));
-	}
-	
-	public String getType() {
-		return type;
-	}
-	
-	public long getValue() {
-		return value;
-	}
-	
-	public void setValue(long value) {
-		this.value= value;
-	}
+import java.util.concurrent.*;
+
+public class Stone {
+    private static final int LOOPS = 25;
+    private static final int MULT = 2024;
+
+    private static final Map<Long, long[]> memoizedSplits = new ConcurrentHashMap<>();
+    private static final Map<Long, Long> memoizedMultiplications = new ConcurrentHashMap<>();
+    private static final Map<Long, long[]> memoizedResults = new ConcurrentHashMap<>();
+
+    private int recursiveCount = 0;
+    private long value;
+
+    public Stone(long value, int count, Test test) {
+        this.value = value;
+        this.recursiveCount = count + 1;
+
+        processStone(value, test, recursiveCount);
+    }
+
+    private void processStone(long currentValue, Test test, int currentCount) {
+        if (currentCount > LOOPS) {
+            test.increaseCount();
+            return;
+        }
+
+        long[] memoized = memoizedResults.get(currentValue);
+        if (memoized != null) {
+            for (long result : memoized) {
+                processStone(result, test, currentCount + 1);
+            }
+            return;
+        }
+
+        if (currentValue == 0) {
+            memoizedResults.put(currentValue, new long[]{1L});
+            processStone(1, test, currentCount + 1);
+        } else if (String.valueOf(currentValue).length() % 2 == 0) {
+            long[] splitValues = memoizedSplits.computeIfAbsent(currentValue, this::split);
+            memoizedResults.put(currentValue, splitValues);
+            for (long result : splitValues) {
+                processStone(result, test, currentCount + 1);
+            }
+        } else {
+            long multipliedValue = memoizedMultiplications.computeIfAbsent(currentValue, v -> v * MULT);
+            memoizedResults.put(currentValue, new long[]{multipliedValue});
+            processStone(multipliedValue, test, currentCount + 1);
+        }
+    }
+
+    private long[] split(long value) {
+        int length = (int) Math.log10(value) + 1;
+        int halfLength = length / 2;
+
+        long divisor = (long) Math.pow(10, halfLength);
+        return new long[]{value / divisor, value % divisor};
+    }
 }
 
-class Tuple<X, Y> { 
-	  public final X x; 
-	  public final Y y; 
-	  public Tuple(X x, Y y) { 
-	    this.x = x; 
-	    this.y = y; 
-	  } 
-	}
+
+
--- src/day11/Test.java
+++ src/day11/Test.java
@@ -1,43 +1,63 @@
 package day11;
 
-import java.awt.Point;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 public class Test {
-	private static int LOOPS = 25;
-	
-	private List<Long> data;
-	private List<Stone> stones = new ArrayList<>();
-	private Processor processor;
+    private static final int THREAD_COUNT = 11; 
+    private final ExecutorService executorService = Executors.newFixedThreadPool(THREAD_COUNT);
 
-	private Test(){
-		data = new Reader().readPointsFromFile();
-		populate();
-		processor = new Processor(stones);
-		
-		for(int i=1; i<LOOPS+1; i++) {
-			processor.cycleProcess();
-			print(i);
-		}
-		System.out.println("\nNumber of stones: " + processor.getStones().size());
-	}
-	
-	public static void main(String[] arg){
-		new Test();
-	}
-	
-	private void populate() {
-		for(Long l: data) {
-			Stone s = new Stone(l);
-			stones.add(s);
-		}
-	}
-	
-	private void print(int loop) {
-		System.out.println("\nBlinck: " + loop);
-		for(Stone s: processor.getStones()) {
-			System.out.print(s.getValue() + " ");
-		}
-	}
+    private List<Long> data;
+    private List<Stone> stones = new ArrayList<>();
+    private long totalStonesCount = 0;
+
+    private ThreadLocal<Long> threadLocalCount = ThreadLocal.withInitial(() -> 0L);
+
+    private Test() {
+        data = new Reader().readPointsFromFile(); 
+        System.out.println(data);
+        populate();
+        System.out.println("\nNumber of stones: " + totalStonesCount);
+    }
+
+    public static void main(String[] args) {
+        new Test();
+    }
+
+    private void populate() {
+        long startTime = System.nanoTime();
+        List<Future<Long>> futures = new ArrayList<>();
+
+        for (Long l : data) {
+            Future<Long> future = executorService.submit(() -> {
+                Stone s = new Stone(l, 1, this);
+                stones.add(s);
+                return threadLocalCount.get();
+            });
+            futures.add(future);
+        }
+
+        for (Future<Long> future : futures) {
+            try {
+                totalStonesCount += future.get();
+            } catch (InterruptedException | ExecutionException e) {
+                e.printStackTrace();
+            }
+        }
+
+        long endTime = System.nanoTime();
+        long durationInMilliseconds = (endTime - startTime) / 1_000_000;
+        System.out.println("Execution time: " + durationInMilliseconds + " milliseconds");
+
+        executorService.shutdown();
+    }
+
+    public void increaseCount() {
+        threadLocalCount.set(threadLocalCount.get() + 1);
+    }
 }
+
